6697031f58000eff144af1397663a732b1454199
Arifjawad18 <arif.jawad18@gmail.com>
2023-02-02 03:42:09 +0600

    edit

--- src/Pages/Eror/Eror.js
+++ src/Pages/Eror/Eror.js
@@ -6,7 +6,7 @@ const Eror = () => {
       <section className="flex items-center h-full p-16 bg-gray-100 text-gray-700">
         <div className="container flex flex-col items-center justify-center px-5 mx-auto my-8">
           <div className="max-w-md text-center">
-            <h2 className="mb-8 font-extrabold text-9xl text-gray-100">
+            <h2 className="mb-8 font-extrabold text-9xl text-gray-700">
               <span className="sr-only">Error</span>404
             </h2>
             <p className="text-2xl font-semibold md:text-3xl">
